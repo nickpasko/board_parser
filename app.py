6afc22c263258650591807ccae9ce7e936669313
Nikolay Pasko <nikolay.pasko@koniglabs.ru>
2019-02-02 13:40:05 +0200

    + folder for temp board files * multiple sequences now work properly (previously onle 1st board_id was stored)

--- app.py
+++ app.py
@@ -14,29 +14,45 @@ app = Flask(__name__)
 api = Api(app)
 app.secret_key = 'LJSHDF*O@Y$Ksw9f;32lirF*(Y&.#LI21r40389gy:>IH34'
 
+storage_path='files/'
 
 @app.route('/board/init', methods=['GET'])
 def init_board():
-    if 'board_ids' not in session:
+    if 'board_ids' not in session or not session['board_ids']:
         print('not in session')
         session['board_ids'] = []
     board_id = str(uuid.uuid4())
-    session['board_ids'].append(board_id)
-    print(len(session))
+    tmp_list = session['board_ids']
+    tmp_list.append(board_id)
+    session['board_ids'] = tmp_list
+    # print(len(session))
     print(len(session['board_ids']))
     return "{'board_id':" + board_id + "}"
 
 
+@app.route('/board/<string:board_id>/check', methods=['GET'])
+def board_check(board_id):
+    print('board_id: '+board_id)
+    if 'board_ids' not in session or not session['board_ids']:
+        return BadRequest('board id list failure')
+    print(session['board_ids'])
+    print(len(session['board_ids']))
+    print('board_ids:')
+    for x in session['board_ids']:
+        print(x)
+    if board_id not in session['board_ids']:
+        return BadRequest('board_id not found')
+    return "{'board_id':" + board_id + "}"
+
 @app.route('/board/<string:board_id>/upload/board', methods=['POST'])
 def upload_board_file(board_id):
-    print(len(session))
     if 'board_ids' not in session:
         return BadRequest('board id list failure')
     if board_id not in session['board_ids']:
         return BadRequest('board_id not found')
     f = request.files['file']
     filename = secure_filename(f.filename)
-    f.save(filename)
+    f.save(storage_path+filename)
     session[board_id] = (filename, '')
 
     return Response('board file uploaded successfully')
@@ -50,7 +66,7 @@ def upload_components_file(board_id):
         return BadRequest('board_id not found')
     f = request.files['file']
     filename = secure_filename(f.filename)
-    f.save(filename)
+    f.save(storage_path+filename)
     session[board_id] = (session[board_id][0], filename)
 
     return Response('components file uploaded successfully')
@@ -68,9 +84,9 @@ def parse_board(board_id):
     if session[board_id][1] == '':
         return BadRequest('components file not uploaded')
 
-    components_file = session[board_id][1]
+    components_file = storage_path+session[board_id][1]
     components_lines = [line.rstrip('\n') for line in open(components_file)]
-    board_file = session[board_id][0]
+    board_file = storage_path+session[board_id][0]
     board_lines = [line.rstrip('\n') for line in open(board_file)]
 
     components = read_components(components_lines)
